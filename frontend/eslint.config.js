4396ddfa7961f29afb3892045da93534ef52429e
Antoine Estienne <estienne.antoine@gmail.com>
2025-09-11 16:18:10 +0200

    fix lint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,19 +1,27 @@
+import eslintPluginAstro from "eslint-plugin-astro";
 import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 
 export default [
+  // TypeScript/React files
   {
     files: ["**/*.{ts,tsx,js,jsx}"],
-    ignores: ["dist", "node_modules", ".astro", "**/*.astro"],
+    ignores: ["dist", "node_modules", ".astro"],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
         ecmaVersion: 2022,
         sourceType: "module",
+        ecmaFeatures: { jsx: true },
+        project: false,
       },
     },
-    linterOptions: {
-      reportUnusedDisableDirectives: true,
+    plugins: {
+      "@typescript-eslint": tsPlugin,
     },
     rules: {},
   },
+
+  // Astro files
+  ...eslintPluginAstro.configs.recommended,
 ];
