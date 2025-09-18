0776b784b728de3125235597518948e8d60dbac7
Antoine Estienne <estienne.antoine@gmail.com>
2025-09-18 15:39:04 +0200

    fix firefox bug issue #43

--- frontend/src/lib/wagmi.ts
+++ frontend/src/lib/wagmi.ts
@@ -1,12 +1,5 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import {
-  mainnet,
-  sepolia,
-  arbitrum,
-  arbitrumSepolia,
-  polygon,
-  polygonAmoy,
-} from "wagmi/chains";
+import { polygonAmoy } from "wagmi/chains";
 
 const projectId = import.meta.env.PUBLIC_WALLET_CONNECT_PROJECT_ID as
   | string
@@ -15,6 +8,6 @@ console.log(projectId);
 export const config = getDefaultConfig({
   appName: "The Guild Genesis",
   projectId: projectId ?? "",
-  chains: [mainnet, sepolia, arbitrum, arbitrumSepolia, polygon, polygonAmoy],
+  chains: [polygonAmoy],
   ssr: false,
 });
